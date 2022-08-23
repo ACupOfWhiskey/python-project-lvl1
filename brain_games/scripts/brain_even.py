bb5f086b1da2a1401e8db738991433351f791eac
ACupOfWhiskey <mia.nishida@gmail.com>
2022-08-23 12:00:15 +0300

    totally working brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -10,14 +10,14 @@ def main():
     i = 0
     win_score = 3
     while i < win_score:
-        random_number = randint(1, 100)
+        random_number = randint(0, 100)
         print(f"Question: {random_number}")
         user_answer = prompt.string("Your answer: ")
         if (user_answer == 'yes' and random_number % 2 == 0) or (user_answer == 'no' and random_number % 2 != 0):
             print("Correct!")
             i += 1
-        elif user_answer == "yes" and random_number % 2 != 0:
-            return (print(f'''"yes" is wrong answer ;(. Correct answer was "no".\nLet's try again, {user_name}!'''))
-        elif user_answer == "no" and random_number % 2 == 0:
-            return (print(f'''"no" is wrong answer ;(. Correct answer was "yes".\nLet's try again, {user_name}!'''))
+        elif user_answer == "yes" or "..." and random_number % 2 != 0:
+            return (print(f'''"{user_answer}" is wrong answer ;(. Correct answer was "no".\nLet's try again, {user_name}!'''))
+        elif user_answer == "no" or "..."  and random_number % 2 == 0:
+            return (print(f'''"{user_answer}" is wrong answer ;(. Correct answer was "yes".\nLet's try again, {user_name}!'''))
     print(f"Congratulations, {user_name}!")
