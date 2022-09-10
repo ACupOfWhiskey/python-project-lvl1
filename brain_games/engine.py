e756aa15a64262fbf84e56fd5c25d3c40d1b9714
ACupOfWhiskey <mia.nishida@gmail.com>
2022-09-10 22:41:54 +0300

    working with engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -18,7 +18,7 @@ def run_game(game):
             print(
                 f'"{user_answer}" is wrong answer ;(. '
                 f'Correct answer was "{correct_answer}".\n'
-                f"Let's try again, {user_name}!")
+                f'Let\'s try again, {user_name}!')
             return
     else:
         print(f'Congratulations, {user_name}!')
