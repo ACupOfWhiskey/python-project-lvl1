f427dd396d4f2e5733a34dbe03fbea6a98c83c63
ACupOfWhiskey <mia.nishida@gmail.com>
2022-10-10 19:07:00 +0300

    final fix

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -15,18 +15,27 @@ END_LENGTH = 100
 START_STEP = 2
 # progression step (end of range)
 END_STEP = 10
+# missed index (start of range)
+INDEX_START = 0
+# missed index (end of range)
+INDEX_END = 5
 
 
-def get_progression():
-    start = randint(START_RANGE, END_RANGE)
-    length = randint(START_LENGTH, END_LENGTH)
-    step = randint(START_STEP, END_STEP)
-    return list(range(start, length, step))
+def get_progression(start, length, step):
+    numbers = []
+    for i in range(start, length, step):
+        numbers.append(i)
+    random_index = randint(INDEX_START, INDEX_END)
+    correct_answer = str(numbers[random_index])
+    numbers[random_index] = '..'
+    # determine the ammount of numbers in a progression
+    string = ' '.join(map(str, numbers[INDEX_START:INDEX_END]))
+    return string, correct_answer
 
 
 def get_question_and_answer():
-    progression = get_progression()
-    correct_answer = str(choice(progression))
-    progression = ' '.join(str(i) for i in progression)
-    question = progression.replace(correct_answer, "..")
+    start = randint(START_RANGE, END_RANGE)
+    step = randint(START_STEP, END_STEP)
+    length = randint(START_LENGTH, END_LENGTH)
+    question, correct_answer = get_progression(start, length, step)
     return question, correct_answer
