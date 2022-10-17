7d925d2c4afb4b5ec3d558a31090e5f52c21f253
ACupOfWhiskey <mia.nishida@gmail.com>
2022-10-17 23:19:18 +0300

    progression mutation

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -1,4 +1,5 @@
 from random import randint
+from random import choice
 
 
 TASK = 'What number is missing in the progression?'
@@ -7,34 +8,30 @@ START_RANGE = 1
 # from which number to start the progression (end of range)
 END_RANGE = 10
 # progression length (start of range)
-START_LENGTH = 80
+START_LENGTH = 5
 # progression length (end of range)
-END_LENGTH = 100
+END_LENGTH = 10
 # progression step (start of range)
-START_STEP = 2
-# progression step (end of range)
-END_STEP = 10
+START_STEP = 1
 # missed index (start of range)
-INDEX_START = 0
-# missed index (end of range)
-INDEX_END = 5
+END_STEP = 10
 
 
 def get_progression(start, length, step):
-    numbers = []
-    for i in range(start, length, step):
-        numbers.append(i)
-    random_index = randint(INDEX_START, INDEX_END)
-    correct_answer = str(numbers[random_index])
-    numbers[random_index] = '..'
-    # determine the ammount of numbers in a progression
-    string = ' '.join(map(str, numbers[INDEX_START:INDEX_END]))
-    return string, correct_answer
+    progression = []
+    for i in range(length):
+        progression.append(str(start + step * i))
+    return progression
 
 
 def get_question_and_answer():
     start = randint(START_RANGE, END_RANGE)
-    step = randint(START_STEP, END_STEP)
     length = randint(START_LENGTH, END_LENGTH)
-    question, correct_answer = get_progression(start, length, step)
+    step = randint(START_STEP, END_STEP)
+    progression = get_progression(start, length, step)
+    random_index = choice(range(len(progression)))
+    missing_member = progression[random_index]
+    progression[random_index] = '..'
+    question = ' '.join(progression)
+    correct_answer = str(missing_member)
     return question, correct_answer
